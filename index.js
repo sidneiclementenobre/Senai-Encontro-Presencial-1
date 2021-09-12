d51322eed5fa7e4272db68680544948f5e53d63b
sidneiclementenobre <sidneiclementenobre@gmail.com>
2021-09-12 20:47:58 -0300

    adicionar git hub

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
-var nomepeça = 3
+var peso = 100
 
-if(nomepeça <= 3) {
-    console.log("A peça tem menos que 3 caracteres em seu nome")
-} else {  
-    console.log("A peça tem 3 ou mais caracteres em seu nome")
+if(peso >= 100) {
+    console.log("Cadastrar Peças")
+} else {
+    console.log("Erro a Peça tem o seu peso inferior a 100g")
+} 
+
+
+var lista = 10
+
+if(lista >= 10) {
+    console.log("A capacidade da caixa nao e suficiente para armazenar as peças")
+} else {
+    console.log("A capacidade da caixa e suficiente para armazenar as peças")
+}
+
+
+var nomePeça = 3
+
+if(nomePeça <= 3) {
+    console.log("A peça tem em seu nome menos de 3 caracteres")
+} else { 
+    console.log("A peça tem em seu nome mais de 3 caracteres")
 }
