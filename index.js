b3d255dfeafb4c99f2daf1b0e59a3aa1c399b251
sidneiclementenobre <sidneiclementenobre@gmail.com>
2021-09-12 18:28:58 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+var nomepeça = 3
+
+if(nomepeça <= 3) {
+    console.log("A peça tem menos que 3 caracteres em seu nome")
+} else {  
+    console.log("A peça tem 3 ou mais caracteres em seu nome")
+}
