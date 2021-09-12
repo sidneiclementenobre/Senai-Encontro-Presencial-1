a723efc0454f8e5319c0c05d80f8b68ba57207c9
sidneiclementenobre <89751785+sidneiclementenobre@users.noreply.github.com>
2021-09-12 20:55:27 -0300

    Update index.js

--- index.js
+++ index.js
@@ -23,3 +23,19 @@ if(nomePeça <= 3) {
 } else { 
     console.log("A peça tem em seu nome mais de 3 caracteres")
 }
+
+Copyright (C) Microsoft Corporation. Todos os direitos reservados.
+
+Experimente a nova plataforma cruzada PowerShell https://aka.ms/pscore6
+
+PS C:\Users\Erica Nobre\Documents\Senai-Encontro Presencial 1> node index.js
+Cadastrar Peças
+PS C:\Users\Erica Nobre\Documents\Senai-Encontro Presencial 1> node index.js
+Cadastrar Peças
+A capacidade da caixa nao e suficiente para armazenar as peças
+PS C:\Users\Erica Nobre\Documents\Senai-Encontro Presencial 1>
+PS C:\Users\Erica Nobre\Documents\Senai-Encontro Presencial 1> node index.js
+Cadastrar Peças
+A capacidade da caixa nao e suficiente para armazenar as peças
+A peça tem em seu nome menos de 3 caracteres
+PS C:\Users\Erica Nobre\Documents\Senai-Encontro Presencial 1>
