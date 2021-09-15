e6e9488ec0632ce12ca5473b1790ce37c0fa13c9
sidneiclementenobre <sidneiclementenobre@gmail.com>
2021-09-15 01:27:57 -0300

    Encontro presencial 2

--- index.js
+++ index.js
@@ -1,61 +1,21 @@
-var data = 05/09
-const dataAtual = 06/09
+var numero = 0
+var total = 14
 
-if(dataAtual >= data) {
-    console.log("Permitir o evento")
-} else {
-    console.log("Data invalida o cadastro nao sera permitida")
+for(var contador = 0;contador<=total;contador++) {
+    if(contador == 0) {
+        console.log("O numero e Zero");
+    }else{
+        if(contador%2 == 0) {
+            console.log("O numero " +contador + " e Par");
+        }else{
+            if(contador%2 == 1) {
+                console.log("O numero " +contador+ " e Impar");
+            }else{
+
+            } 
+
+        
+        }
+    
+    }
 }
-
-var  idade = 18 
-
-if(idade >= 18) {
-    console.log("Permitir Cadastro")
-} else {
-    console.log("Cadastro nao permitido pela idade")
-}
-
-
-let listaDeParticipantes = ["Participantes" , "Palestrantes"];
-let quantidadeDeParticipantes = listaDeParticipantes.length;
-for (let indice = 0; indice < quantidadeDeParticipantes; indice++) {
-    const pessoaCorrente = listaDeParticipantes[indice];
-    console.log(pessoaCorrente);
-}
-
-
-var quantidade = 100
-
-if(quantidade <= 100) {
-    console.log("Permitir Cadastro de Participantes")
-} else {
-    console.log("Cadastro nao Permitido por ter excedido o limite")
-
-}
-
-
-
-<<<<<<< HEAD
-=======
-if(nomePeça <= 3) {
-    console.log("A peça tem em seu nome menos de 3 caracteres")
-} else { 
-    console.log("A peça tem em seu nome mais de 3 caracteres")
-}
-
-Copyright (C) Microsoft Corporation. Todos os direitos reservados.
-
-Experimente a nova plataforma cruzada PowerShell https://aka.ms/pscore6
-
-PS C:\Users\Erica Nobre\Documents\Senai-Encontro Presencial 1> node index.js
-Cadastrar Peças
-PS C:\Users\Erica Nobre\Documents\Senai-Encontro Presencial 1> node index.js
-Cadastrar Peças
-A capacidade da caixa nao e suficiente para armazenar as peças
-PS C:\Users\Erica Nobre\Documents\Senai-Encontro Presencial 1>
-PS C:\Users\Erica Nobre\Documents\Senai-Encontro Presencial 1> node index.js
-Cadastrar Peças
-A capacidade da caixa nao e suficiente para armazenar as peças
-A peça tem em seu nome menos de 3 caracteres
-PS C:\Users\Erica Nobre\Documents\Senai-Encontro Presencial 1>
->>>>>>> a723efc0454f8e5319c0c05d80f8b68ba57207c9
