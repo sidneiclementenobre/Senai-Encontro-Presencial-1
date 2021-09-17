16b3adb588196efd4031844e5ca83b01cdb41195
sidneiclementenobre <89751785+sidneiclementenobre@users.noreply.github.com>
2021-09-16 21:36:22 -0300

    Update index.js

--- index.js
+++ index.js
@@ -31,33 +31,9 @@ Copyright (C) Microsoft Corporation. Todos os direitos reservados.
 Experimente a nova plataforma cruzada PowerShell https://aka.ms/pscore6
 
 PS C:\Users\Erica Nobre\Documents\Senai-Encontro Presencial 1> node index.js
-O numero e Zero
-O numero 1 e Impar
-O numero 2 e Par  
-O numero 3 e Impar
-O numero 4 e Par  
-O numero 5 e Impar
-O numero 6 e Par
-O numero 7 e Impar
-O numero 8 e Par
-O numero 9 e Impar
-O numero 10 e Par
-O numero 11 e Impar
-O numero 12 e Par
-O numero 13 e Impar
-O numero 14 e Par
- O numero e Zero
-O numero e Impar1
-O numero e Par2
-O numero e Impar3
-O numero e Impar5
-O numero e Par6
-O numero e Impar7
-O numero e Par8
-O numero e Impar9
-O numero e Par10
-O numero e Impar11
-O numero e Par12
-O numero e Impar13
-O numero e Par14
+Permitir Cadastro
 PS C:\Users\Erica Nobre\Documents\Senai-Encontro Presencial 1> node index.js
+A capacidade da caixa nao e suficiente para armazenar as peças
+PS C:\Users\Erica Nobre\Documents\Senai-Encontro Presencial 1> node index.js
+A peça tem em seu nome menos de 3 caracteres
+PS C:\Users\Erica Nobre\Documents\Senai-Encontro Presencial 1>
